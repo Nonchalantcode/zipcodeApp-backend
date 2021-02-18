0334f4d0a74ead3631180b13f68bb6e8b088749b
Nonchalantcode <hectorg269s@outlook.com>
2021-02-17 19:55:04 -0600

    Refactored index.js to make it an AWS lambda function to retrieve suggestions for a zipcode

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ exports.handler = async (event) => {
     const zip = event['zip']
     const req = await axios.get(BASE_URL)
     const data = req['data']
-    const results = data['zipCodes']
-    if(results[zip] === undefined) {
-        return { 'zip code': zip, ...results }
-    } else {
-        return { 'zip code': zip, ...results[zip] }
+    const suggestions = []
+    const limit = 5 
+    for(let i = 0, len = data.all.length; i < len; i++) {
+        const currentZipCode = data.all[i]
+        if(suggestions.length === limit) break
+        if(currentZipCode.startsWith(zip)) suggestions.push(currentZipCode)
     }
+    return { suggestions }
 }
