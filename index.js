e0a28d636a999ff691fd3c67c05788c78c4dd79a
Nonchalantcode <hectorg269s@outlook.com>
2021-02-19 12:18:33 -0600

    Refactored lambda function to make a query for suggestions to a DynamoDB instance

--- index.js
+++ index.js
@@ -1,18 +1,68 @@
 require('dotenv').config()
-const axios = require('axios')
+var AWS = require("aws-sdk");
 
-const BASE_URL = process.env.BASE_URL;
+AWS.config.update({
+    region: process.env['REGION'],
+    endpoint: process.env['DB_ENDPOINT']
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+function calcZipcodeRange(zipcode) {
+    /* The frontend enforces that zipcodes suggestions will be shown only if the user has specified at least the first 3 digits */
+    typeof zipcode === 'number' 
+        ? zipcode = `${zipcode}`
+        : (() => {})()
+    switch(zipcode.length) {
+        /* If we have only the first three numbers of a zipcode */
+        case 3: {
+            zipcode = zipcode + '00'
+            const min = parseInt(zipcode)
+            return [min, min + 10]
+        }
+        case 4: {
+            zipcode = zipcode + '0'
+            const min = parseInt(zipcode)
+            return [min, min + 10]
+        }
+        default: 
+            return [parseInt(zipcode), parseInt(zipcode) + 10]
+    }
+}
 
 exports.handler = async (event) => {
-    const zip = event['zip']
-    const req = await axios.get(BASE_URL)
-    const data = req['data']
-    const suggestions = []
-    const limit = 5 
-    for(let i = 0, len = data.all.length; i < len; i++) {
-        const currentZipCode = data.all[i]
-        if(suggestions.length === limit) break
-        if(currentZipCode.startsWith(zip)) suggestions.push(currentZipCode)
+    const zipcode = event['zip']
+    let [min, max] = calcZipcodeRange(zipcode)
+    var params = {
+        TableName: 'zipcodes',
+        ProjectionExpression: "#zipnum",
+        FilterExpression: "#zipnum between :min and :max",
+        ExpressionAttributeNames: {
+            "#zipnum": "zipcode"
+        },
+        ExpressionAttributeValues: {
+            ":min": min,
+            ":max": max
+        },
     }
-    return { suggestions }
+
+    try {
+        const data = await new Promise((resolve, reject) => {
+            docClient.scan(params, (err, data) => {
+                if(err) {
+                    reject(err)
+                } else {
+                    let results = 
+                        data.Items.reduce((acc, {zipcode}) => {
+                            acc.push(String(zipcode))
+                            return acc
+                        }, [])
+                    resolve(results.sort())
+                }
+            })
+        })
+        return { "suggestions": data }
+    } catch (err) {
+        return { "suggestions": [] }
+    }
 }
