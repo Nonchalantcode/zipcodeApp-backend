44dac8b4e2ae94252bd8373aa95d575f0c23052f
Nonchalantcode <hectorg269s@outlook.com>
2021-02-19 10:48:40 -0600

    Changed lambda to query the corresponding zipcode from DynamoDB

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 require('dotenv').config()
-const axios = require('axios')
+var AWS = require("aws-sdk");
 
-const BASE_URL = process.env.BASE_URL;
+AWS.config.update({
+    region: process.env['REGION'],
+    endpoint: process.env['DB_ENDPOINT']
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
-    const zip = event['zip']
-    const req = await axios.get(BASE_URL)
-    const data = req['data']
-    const results = data['zipCodes']
-    if(results[zip] === undefined) {
-        return { 'zip code': zip, message: `Zip code ${zip} isn't valid/registered.` }
-    } else {
-        return { 'zip code': zip, ...results[zip] }
+    const zipcode = parseInt(event['zip'])
+    var params = {
+        TableName: 'zipcodes',
+        Key:{
+            'zipcode': zipcode
+        }
+    };
+
+    try {
+        const data = await new Promise((resolve, reject) => {
+                                    docClient.get(params, function(err, data) {
+                                        if (err) {
+                                            reject(null)
+                                        } else {
+                                            resolve(data)
+                                        }
+                                    });
+                                })
+        let { Item: { info: { department, municipality, neighbourhood } }} = data
+        let results = { 'zip code': zipcode, department, municipality, neighbourhood }
+        return results
+    } catch (ex) {
+        if(ex.name === 'TypeError') {
+            return { 'zip code': zipcode, message: `Zip code ${zipcode} isn't valid or registered`}
+        } else {
+            return { 'zip code': zipcode, message: ex.message }
+        }
     }
 }
